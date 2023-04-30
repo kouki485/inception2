5b8b0b8d2f84538d84d98b63c05cc1cc938a1593
kkohki <mwd3145@gmail.com>
2023-04-30 19:31:11 +0900

    fix2

--- Inception/srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ Inception/srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -6,7 +6,7 @@ else
     sleep 5
     wp core config --allow-root --dbname=wordpress --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb:3306
     wp core install --allow-root --url='kkohki.42.fr' --title='WordPress for Inception' --admin_user=master --admin_password=master --admin_email="admin@admin.fr"
-    wp user create --allow-root username user2@user.com --user_pass=1234567890 --role=author
+    wp user create --allow-root kkohki user2@user.com --user_pass=1234567890 --role=author
 fi
 
 php-fpm7.4 --nodaemonize
