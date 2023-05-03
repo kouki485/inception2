d6b0c84d326f654abe1154b81109067062e31604
kkohki <mwd3145@gmail.com>
2023-05-03 11:29:09 +0900

    final fix

--- Inception/srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,16 +1,13 @@
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-    apt-get install -y mariadb-server
 
     mysql_install_db
     service mysql start
 
+    mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWORD}';" &&
+        mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&
+        mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" &&
+        mysql -e "FLUSH PRIVILEGES;"
     mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
-
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-
     service mysql stop
 fi
 mysqld --bind-address=0.0.0.0
